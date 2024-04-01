23b6147c474d1629c0cad7c663b3f3b211632204
Mario Contreras <mariocc29@gmail.com>
2024-04-01 11:51:54 -0400

    added seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+require 'csv'
+
+CSV.new(
+  URI.open(Rails.application.config.pokemon[:url]), 
+  :headers => :first_row).each do |line|
+    Pokemon.create({
+      name: line[1],
+      primary_type: line[2],
+      secondary_type: line[3],
+      total: line[4],
+      hp: line[5],
+      attack: line[6],
+      defense: line[7],
+      sp_atk: line[8],
+      sp_def: line[9],
+      speed: line[10],
+      generation: line[11],
+      legendary: line[12]
+    })
+end
